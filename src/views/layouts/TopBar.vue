abface33ecbf5e5c36fd3528b925985f56d8a6ab
Thiago Billy <thiagobilly27@gmail.com>
2023-03-06 19:18:37 -0300

    refact: links on the topbar

--- src/views/layouts/TopBar.vue
+++ src/views/layouts/TopBar.vue
@@ -4,7 +4,7 @@
       <v-toolbar-title class="mr-2">
         <strong>STOCK</strong>TRADER
       </v-toolbar-title>
-      <v-btn-toggle
+      <!-- <v-btn-toggle
         mandatory
         color="black"
         background-color="indigo lighten-4"
@@ -12,7 +12,19 @@
         <v-btn> <router-link to="/">Início</router-link> </v-btn>
         <v-btn> <router-link to="/portifolio">Portifólio</router-link> </v-btn>
         <v-btn> <router-link to="/acoes">Ações</router-link> </v-btn>
-      </v-btn-toggle>
+      </v-btn-toggle> -->
+
+      <router-link to="/">
+        <v-btn large color="transparent" elevation="0">Início </v-btn>
+      </router-link>
+
+      <router-link to="/portifolio" class="mx-2">
+        <v-btn large color="transparent" elevation="0">Portifólio </v-btn>
+      </router-link>
+
+      <router-link to="/acoes">
+        <v-btn large color="transparent" elevation="0">Ações </v-btn>
+      </router-link>
     </div>
 
     <v-spacer></v-spacer>
@@ -33,6 +45,12 @@ export default {};
 .v-application {
   a {
     color: unset;
+    overflow: hidden;
+    border-radius: 10px;
+
+    &.router-link-exact-active {
+      background: #b8b9c3;
+    }
   }
 }
 </style>
